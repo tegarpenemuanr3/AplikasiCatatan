1dc2b010ad62a7256b8812a28a46cffbcfb33aed
Tegar Penemuan <tegarpenemuan@gmail.com>
2022-02-26 21:20:26 +0700

    Add Intent Main to Edit

--- app/src/main/java/com/tegarpenemuan/latihanroomdatabase/MainActivity.kt
+++ app/src/main/java/com/tegarpenemuan/latihanroomdatabase/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.tegarpenemuan.latihanroomdatabase
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupListener()
+    }
+
+    private fun setupListener() {
+        button_create.setOnClickListener {
+            startActivity(Intent(this,EditActivity::class.java))
+        }
     }
 }
