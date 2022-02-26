457fc2d302e4902f8d6fd3d7a3a3c7e6e811feae
Tegar Penemuan <tegarpenemuan@gmail.com>
2022-02-26 20:25:06 +0700

    Note Dao

---
+++ app/src/main/java/com/tegarpenemuan/latihanroomdatabase/room/NoteDao.kt
@@ -0,0 +1,18 @@
+package com.tegarpenemuan.latihanroomdatabase.room
+
+import androidx.room.*
+
+@Dao
+interface NoteDao {
+    @Insert
+    suspend fun addNote(note:Note)
+
+    @Update
+    suspend fun updateNote(note: Note)
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+
+    @Query("SELECT * FROM note")
+    suspend fun getNotes(): List<Note>
+}
